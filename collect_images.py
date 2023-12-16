6200cdeb7fbb6f5ea0b520d183002b38db97b89b
MahamPervez <maham.pervez.456@gmail.com>
2023-12-16 14:08:46 +0500

    Initial commit

---
+++ collect_images.py
@@ -0,0 +1,51 @@
+import os
+import cv2
+
+DATA_DIR = './data'
+if not os.path.exists(DATA_DIR):
+    os.makedirs(DATA_DIR)
+
+number_of_classes = 3
+dataset_size = 100
+
+# Find an available camera index
+camera_index = None
+for i in range(4):  # Try indices from 0 to 3q
+    cap = cv2.VideoCapture(i)
+    if cap.isOpened():
+        camera_index = i
+        break
+
+if camera_index is None:
+    print("No available camera found.")
+    exit()
+
+cap = cv2.VideoCapture(camera_index)
+
+for j in range(number_of_classes):
+    class_dir = os.path.join(DATA_DIR, str(j))
+    if not os.path.exists(class_dir):
+        os.makedirs(class_dir)
+
+    print('Collecting data for class {}'.format(j))
+
+    done = False
+    while True:
+        ret, frame = cap.read()
+        cv2.putText(frame, 'Ready? Press "Q" ! :)', (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3,
+                    cv2.LINE_AA)
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(25) == ord('q'):
+            break
+
+    counter = 0
+    while counter < dataset_size:
+        ret, frame = cap.read()
+        cv2.imshow('frame', frame)
+        cv2.waitKey(25)
+        cv2.imwrite(os.path.join(class_dir, '{}.jpg'.format(counter)), frame)
+
+        counter += 1
+
+cap.release()
+cv2.destroyAllWindows()
